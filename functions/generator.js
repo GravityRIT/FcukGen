801295f985ccc6888a6ae21d26ad152ca6bb9e0e
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2018-05-22 08:45:15 +0530

    added THE FUCK

--- functions/generator.js
+++ functions/generator.js
@@ -1,11 +1,14 @@
 const pos = require('pos');
 
+const noun = /^NN/;
+const verb = /^VB/;
+
 let produce =  function(doc) {
     const words = new pos.Lexer().lex(doc);
     const tagger = new pos.Tagger();
     const taggedWords = tagger.tag(words);
     let result = "";
-    let status = 0;
+    let prevT='', prevW='';
     let ctr = 0,j;
     for (let i in taggedWords) {
         let taggedWord = taggedWords[i];
@@ -17,19 +20,23 @@ let produce =  function(doc) {
         doc = doc.substr(j + word.length);
         for(;j>0;j--)
             result += " ";
-        if(tag === "NN" || tag === "NNS") {
-            if(status !== 1){
-                status = 1;
-                result += "fucking ";
-            }
-        }
-        else {
-            status = 0;
-        }
+        if(noun.exec(tag) && !noun.exec(prevT))
+            result += "fucking ";
+        /*else if(verb.exec(tag));*/
+        else if(word.toUpperCase() === 'IS' || word.toUpperCase() === 'ARE')
+            if(isQue(prevW))
+                result += "the fuck ";
+
         result += word;
+        prevT = tag;
+        prevW = word;
     }
     console.log(result);
     return result;
 };
 
+function isQue(str) {
+    str = str.toUpperCase();
+    return str === "WHAT" || str === "WHERE" || str === "WHO" || str === "HOW";
+}
 module.exports = produce;
